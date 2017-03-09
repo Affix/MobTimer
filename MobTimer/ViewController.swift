a74481f41ccf3641594404fb0c5c78707d2841a8
Keiran Smith <affix@affix.me>
2017-03-09 20:49:44 +0000

    Initial Commit

---
+++ MobTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MobTimer
+//
+//  Created by Keiran Smith on 09/03/2017.
+//  Copyright © 2017 Keiran Smith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
