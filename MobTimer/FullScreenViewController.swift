ae54dc252cb1f22b595fa9de79550e715b4067ae
Keiran Smith <affix@affix.me>
2017-03-10 22:35:00 +0000

    Make FullScreenView window close

--- MobTimer/FullScreenViewController.swift
+++ MobTimer/FullScreenViewController.swift
@@ -12,6 +12,8 @@ class FullScreenViewController: NSViewController {
     
     @IBOutlet weak var message: NSTextField!
     
+    @IBOutlet var FullView: NSView!
+    
     let global = Global()
     
     override func viewDidAppear() {
@@ -22,11 +24,20 @@ class FullScreenViewController: NSViewController {
         self.view.enterFullScreenMode(NSScreen.main()!, withOptions:optionsDictionary)
         self.view.wantsLayer = true
         self.view.layer?.backgroundColor = NSColor.black.cgColor
+        
         message.stringValue = "Hello \(global.nextMobber), time to fly!\nPress any key to continue"
     }
     
     override func keyDown(with event: NSEvent) {
         // Do stuff to dismiss view
+        
+        FullView.isHidden = true
+        let presOptions: NSApplicationPresentationOptions = ([.fullScreen,.autoHideMenuBar])
+        let optionsDictionary = [NSFullScreenModeApplicationPresentationOptions :
+            NSNumber(value: presOptions.rawValue)]
+        self.view.exitFullScreenMode(options: optionsDictionary)
+        self.view.wantsLayer = false
+        FullView.window?.close()
     }
     
     func performSegue(for segue: NSStoryboardSegue, withIdentifier identifier: String, sender: Any?) {
